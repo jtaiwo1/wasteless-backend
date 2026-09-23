a52435e4f9e39694a9d76862e0237c467f3196de
unknown <ismaeelhaque@gmail.com>
2026-09-23 11:36:28 +0100

    Forgot to add the return for the actual JSON

--- insights/app/main.py
+++ insights/app/main.py
@@ -20,24 +20,26 @@ def analytics(items: list[dict]):
     total_donated = int(df[df['status'] == 'donated']['quantity'].sum())
     total_wasted = int(df[df['status'] == 'wasted']['quantity'].sum())
     total_used = int(df[df['status'] == 'used']['quantity'].sum())
-    total_available = int(df[df['status'] == 'availible']['quantity'].sum())
+    total_available = int(df[df['status'] == 'available']['quantity'].sum())
 
     # Analytics percentage
-    donated_percentage = round((total_donated / total_quantity) * 100, 2)
-    wasted_percentage = round((total_wasted / total_quantity) * 100, 2)
-    used_percentage = round((total_used / total_quantity) * 100, 2) 
-    available_percentage = round((total_availible / total_quantity) * 100, 2) 
+    donated_percentage = round((total_donated / total_quantity) * 100, 2) if total_quantity > 0 else 0
+    wasted_percentage = round((total_wasted / total_quantity) * 100, 2) if total_quantity > 0 else 0
+    used_percentage = round((total_used / total_quantity) * 100, 2) if total_quantity > 0 else 0
+    available_percentage = round((total_available / total_quantity) * 100, 2) if total_quantity > 0 else 0
 
-
-
-    
+    # Advanced
 
 
     
     return {
-        "total_items": len(items),
-        "available": sum(item.get("status") == "available" for item in items),
-        "donated": sum(item.get("status") == "donated" for item in items),
-        "used": sum(item.get("status") == "used" for item in items),
-        "wasted": sum(item.get("status") == "wasted" for item in items)
+        "total_items": int(total_quantity),
+        "total_donated": total_donated,
+        "donated_percentage": donated_percentage,
+        "total_wasted": total_wasted,
+        "wasted_percentage": wasted_percentage,
+        "total_used": total_used,
+        "used_percentage": used_percentage,
+        "total_available": total_available,
+        "available_percentage": available_percentage,
     }
