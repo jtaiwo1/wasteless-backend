c382eb47f5d5710aafb993e45165e5fdb6033dab
unknown <ismaeelhaque@gmail.com>
2026-09-23 16:28:54 +0100

    Givefood API is working extracting data like name, distance, needs, and address

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 // const pantryRouter = require("./routers/pantry");
-// const charitiesRouter = require("./routers/charities");
+const charitiesRouter = require("./routers/charities");
 // const dashboardRouter = require("./routers/dashboard");
 const userRouter = require("./routers/users")
 
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use("/users", userRouter)
 app.get("/health", (req, res) => res.json({ status: "ok" }));
 // app.use("/pantry", pantryRouter);
-// app.use("/charities", charitiesRouter);
+app.use("/api/charities", charitiesRouter);
 // app.use("/dashboard", dashboardRouter);
 module.exports = app;
